2e30781e9b322089b9159a517febce74da430bfb
law <lawuche29@gmail.com>
2022-07-08 17:46:16 +0100

    changed stuff

--- react_django_notes/settings.py
+++ react_django_notes/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'react_django_notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"notes-frontend/build")],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[
-  os.path.join(BASE_DIR,"notes-frontend/build/static")
+  os.path.join(BASE_DIR,"static")
 ]
 
 # Default primary key field type
